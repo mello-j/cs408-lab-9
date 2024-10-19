11c91476b8a829ae54c6f532f1033144cf31293e
Justin Mello <justinmello@u.boisestate.edu>
2024-10-18 23:22:30 -0600

    Finished everything but additional tests

--- README.md
+++ README.md
@@ -2,23 +2,41 @@
 
 ## Overview
 
-TODO: Write a project description
+Justin Mello  
+CS 408  
+Fall 2024  
+Module 9: Lab 9.2  
+
+# Project Description
+In this project we built a very simple javascript browser based game, using a windows event listener to track user keyboard input as "controls" for an evil circle. This is an extension of the bouncy ball demo that demonstrates object oriented programming in javascript.
+
+## Running the Website
+
+1. To run everything you will need to Fork or Clone this repository and open in up in an IDE on your local PC or in VS Codespaces.
+
+2. Once you have cloned the Repo, open it up in VS Code.
+
+3. Select the index.html file from the Explorer Window.
+
+4. Then, using the VS Code Command Palette, type: > Live Preview Start Server. If this command is not available, you may need to install the VS Code Live Preview extension.
+
+5. This will open up a website preview within your IDE. You can also open this in an external browser if you choose, by clicking on the 3 horizontal lines icon to the top right of the preview window and selecting 'Open in Browser'.
+
+6. Play EvilCircle! To play evil circle, use the WSAD keys on your keybaord to move in the respective directions "W" = Up, S = Down, A = Left,  D = Right.
+
+7. Keep track of the Ball Count at the top right of your screen for your score. When the evil Circle eats all the balls you win!
+
+8. Note the evil circle (Mr. Stay Puft Marshmallow man) will grow with each ball you eat, however it's speed will also decrease to create a different challenge!
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
-
 A simple bulleted list below is sufficient. For example:
 
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+- JavaScript Draw Image: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+- Drawing Graphics: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Drawing_graphics
+- JavaScript Objects: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
+- JavaScript Classes: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+## Reflection
+This was a good review of javascript fundamentals, and an advanced dive into object oriented programming in javascript (Which I've touched on slightly in personal endeavors).
+This lab was really fun, and I added a few things (like an image instead of the evil circle) but started this a bit late and didn't finish all the things I wanted. However I do plan to play around a bit more and see just where I could possibly take this (and/or other ideas). This was a great creative ground to learn new things and I really enjoyed this lab!
--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-// set up canvas
+//global variables for gameplay
+let gamePlay;
+let balls;
+let evilCircle;
 
+// set up canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// grab paragraph
+const p = document.querySelector("p");
+let scoreCount = 0;
+
+//Need to finish....add a game over check and a button to start/stop.
+function checkGameOver() {
+  if (scoreCount === 0){
+    gameRunning = false;
+  }
+}
 
 // function to generate random number
 
@@ -18,22 +33,34 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+//New Shape Class
+class Shape {
+
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+//Updated ball class extends Shape
+class Ball extends Shape {
+
+  constructor(x, y, velX, velY, color, size, exists) {
+    super(x, y, velX, velY)
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+  draw(){
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();    
   }
+  
 
   update() {
     if (this.x + this.size >= width) {
@@ -56,9 +83,11 @@ class Ball {
     this.y += this.velY;
   }
 
+  
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      //updated to check that ball exists
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -71,35 +100,164 @@ class Ball {
   }
 }
 
-const balls = [];
+//Adding in EvilCircle class that inherits from Shaope
+class EvilCircle extends Shape{
 
-while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size
-  );
+  constructor(x,y) {
+    super(x, y, 50, 50)
+    this.color = "white";
+    this.size = 25;
+    this.maxSize = 200;
 
-  balls.push(ball);
+    //stay puft man
+    this.image = new Image();
+    this.image.src = 'img/staypuft.jpg';
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  //Evil circle draw method
+  draw() {
+    //draw mr. puft
+      if (this.image.complete) {
+        ctx.drawImage(
+          this.image,
+          this.x - this.size,
+          this.y - this.size,
+          this.size * 2,
+          this.size * 2
+        );
+      } else {
+      //change Evil circle fill and outline
+      ctx.beginPath();
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+
+  //Evil Circle checkbounds method adapted from ball.update
+  //Todo - update the means to calculate evilCircle
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
+    }
+  }
+
+  //EvilCircle Collision Detect method
+  collisionDetect() {
+    for (const ball of balls) {
+      //updated to check that ball exists
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        
+        //updated to set ball.exists to false
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+       
+          //ball eaten, evil circle growzzzz and evil circle slowzzzz
+          if(this.size < this.maxSize){
+            this.size+= this.size / 5
+            this.size = Math.min(this.size, this.maxSize);
+            this.velX -= this.velX / 20;
+            this.velY -= this.velY / 20;
+          }
+
+          scoreCount--;
+          p.textContent = "Ball count: " + scoreCount;
+
+          if (scoreCount == 0){
+
+          }
+        }
+      }
+    }
+  }
+}
+
+//start gamePlay
+function playGame() {
+  scoreCount = 0;
+  balls = [];
+  gamePlay = true;
+  evilCircle = new EvilCircle(random(0,canvas.width), random(0,canvas.height));
+
+
+  while (balls.length < 25) {
+    const size = random(10, 20);
+    const ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas, to avoid drawing errors
+      random(0 + size, width - size),
+      random(0 + size, height - size),
+      random(-7, 7),
+      random(-7, 7),
+      randomRGB(),
+      size
+    );
+  
+    balls.push(ball);
+    scoreCount++;
+    p.textContent = "Ball count: " + scoreCount;
+  }
+
+  loop();
 }
 
 function loop() {
+  if (!gamePlay){
+    return;
+  }
+
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    //Only draw if ball exists to account for eaten balls
+    if (ball.exists){
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
 
+   //Draw Evil Circle objecty evilCircle, bring into the program
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
+  checkGameOver();
   requestAnimationFrame(loop);
 }
 
-loop();
+playGame();
